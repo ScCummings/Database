516cb1a7fb01f320f5a25d0f20e56c442d3fd3db
Scott Cummings <scummings@chapman.edu>
2019-04-22 22:30:47 -0700

    Updated accessors and mutators for the student class.

--- Student.cpp
+++ Student.cpp
@@ -1,19 +1,44 @@
 // accessor for the student's advisor's ID
-int get_advisorID(){
+int GetAdvisorID(){
     return advisorID;
 }
 
 // the accessor for the student's GPA
-float get_GPA(){
+float GetGPA(){
     return GPA;
 }
 
+// accessor for the student's ID
+int GetID(){
+    return id;
+}
+
+//accessor for the student's name
+string GetName(){
+    return name;
+}
+
+//accessor for the student's rank
+short GetRank(){
+    return rank;
+}
+
+//accessor for the student's specialization
+string GetSpecialization(){
+    return specialization;
+}
+
+//mutator for the student's Specialization (major)
+string SetSpecialization(string specialization){
+    this.specialization = specialization;
+}
+
 // mutator for the student's advisor's ID
-void set_advisorID(int advisorID){
-    advisorID = advisorID;
+void SetAdvisorID(int advisorID){
+    this.advisorID = advisorID;
 }
 
 // mutator for the student's GPA
-void set_GPA(float GPA){
-    GPA = GPA;
+void SetGPA(float GPA){
+    this.GPA = GPA;
 }
