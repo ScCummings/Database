04659e152264cee5791d62ef1635008de3ffd7ba
Dan Haub <haub@chapman.edu>
2019-04-23 04:11:55 +0000

    Removed Person.cpp and reformatted Person.h

--- Person.h
+++ Person.h
@@ -2,35 +2,30 @@
 
 using namespace std;
 
-class Person
-{
-private:
-  // the ID number of the person
- int id;
+class Person{
+    private:
+        // the ID number of the person
+        int id;
+        // the name of the person
+        string name;
+        // the rank of the person (Year for student and rank for faulty)
+        string rank;
+        // the Specialization of the person (major for student and department for faculty)
+        string specialization;
 
- // the name of the person
- string name;
+    public:
+        // accessor for rank
+        virtual string GetRank() = 0;
 
- // the rank of the person (Year for student and rank for faulty)
-  string rank;
+        // accessor for name
+        virtual string GetName() = 0;
 
- // the Specialization of the person (major for student and department for faculty)
- string specialization;
+        // accessor for ID
+        virtual int GetID() = 0;
 
- ////accessors & mutator for Specialization and rank////
-public:
- // accessor for rank
- virtual string GetRank();
+        // accessor for specialization
+        virtual string GetSpecialization() = 0;
 
- // accessor for name
- virtual string GetName();
-
- // accessor for ID
- virtual int GetID();
-
- // accessor for specialization
- virtual string GetSpecialization();
-
- // mutator for specialization
- virtual void SetRank(string specialization);
+        // mutator for specialization
+        virtual void SetRank(string specialization) = 0;
 };
