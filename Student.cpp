8aca6749f0113b4a25cf6fdcb2c519e7f7c76d64
Scott Cummings <scummings@chapman.edu>
2019-04-22 21:40:18 -0700

    Fixed style on Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,23 +1,19 @@
 // accessor for the student's advisor's ID
-int get_advisorID()
-{
-  return advisorID;
+int get_advisorID(){
+    return advisorID;
 }
 
 // the accessor for the student's GPA
-float get_GPA()
-{
-  return GPA;
+float get_GPA(){
+    return GPA;
 }
 
 // mutator for the student's advisor's ID
-void set_advisorID(int advisorID)
-{
-  advisorID = advisorID;
+void set_advisorID(int advisorID){
+    advisorID = advisorID;
 }
 
 // mutator for the student's GPA
-void set_GPA(float GPA)
-{
-  GPA = GPA;
+void set_GPA(float GPA){
+    GPA = GPA;
 }
