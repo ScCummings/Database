73f3c7caf3c3130ed24d284b9d52b1124866393a
Scott Cummings <scummings@chapman.edu>
2019-05-01 00:53:11 -0700

    better implementation of main

--- FileIOTesting2.cpp
+++ FileIOTesting2.cpp
@@ -1,5 +1,5 @@
 #include "FileIO.h"
-
+#include "Database.h"
 int main(){
     FileIO newFileIO;
     bool b = newFileIO.CheckLoadStatus();
@@ -12,5 +12,23 @@ int main(){
         s = "false";
     }
     cout << s << endl;
+    BST<Faculty>* faculty = newFileIO.LoadFaculty();
+    cout << "DEBUG: " << faculty->GetRoot()->key << endl;
+    BST<Student>* students = newFileIO.LoadStudents();
+    Database database(students,faculty);
+    cout << "******DATABASE TESTING*********" << endl;
+    database.PrintFaculty();
+	/*database.PrintStudents();
+	PrintFaculty(int facultyID);
+	void PrintStudent(int studentID);
+	void PrintAdvisor(int studentID);
+	void PrintAdvisees(int facultyID);
+	void AddStudent(Student newStudent);
+	void DeleteStudent(int studentID);
+	void AddFaculty(Faculty newFaculty);
+	void DeleteFaculty(int facultyID);
+	void ChangeAdvisor(int studentID, int facultyID);
+	void RemoveAdvisee(int facultyID, int studentID);
+	void Rollback();*/
     return 0;
 }
