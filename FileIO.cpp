3b8feb262b2af23cac09aa11bb8b03766d21f789
Dan Haub <haub@chapman.edu>
2019-05-01 03:06:12 +0000

    Began implementing fileIO

--- FileIO.cpp
+++ FileIO.cpp
@@ -1,16 +1,19 @@
+#include "FileIO.h"
+
+using namespace std;
+
 void FileIO::Save(BST<Student> studentTree, BST<Faculty> facultyTree){
-    if(node == nullptr){
-        return;
-    }
-    else{
-        //reference to key
-        recPrint(node->left);
-        recPrint(node->right);
-    }
+    
 }
+
 BST<Student> FileIO::LoadStudents(){
 
 }
+
 BST<Faculty> FileIO::LoadFaculty(){
 
+}
+
+bool FileIO::CheckLoadStatus(){
+    
 }
--- FileIO.h
+++ FileIO.h
@@ -1,10 +1,37 @@
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+#include <fstream>
+#include <iostream>
+
+#include "Student.h"
+#include "Faculty.h"
+#include "GenTree.h"
+
+using namespace std;
+
 class FileIO{
     private:
-static ifstream inputStream;
-static ofstream outputStream;
-    public:
-void Save(BST<Student> studentTree, BST<Faculty> facultyTree);
-BST<Student> LoadStudents();
-BST<Faculty> LoadFaculty();
+        //file input stream
+        static ifstream inputStream;
+        //file output stream
+        static ofstream outputStream;
 
+        //Stores string for name of student data file
+        const string studentData = "StudentTable.dat";
+        //Stores string for name of student data dump file
+        const string studentDataDump = "StudentTableDump.dat";
+
+        //Stores string for name of faculty data file
+        const string facultyData = "FacultyTable.dat";
+        //Stores string for name of facluty data dump file
+        const string facultyDataDump = "FacultyTableDump.dat";
+
+    public:
+        bool CheckLoadStatus();
+        void Save(BST<Student> studentTree, BST<Faculty> facultyTree);
+        BST<Student> LoadStudents();
+        BST<Faculty> LoadFaculty();
 };
+
+#endif //FILE_IO_H
